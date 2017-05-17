6f85193fb60cbf316b692afc9d98bdd99a846db2
katsuhitomuratomi <garo0713@icloud.com>
2017-05-17 20:46:35 +0900

    commit1

---
+++ app/src/main/java/jp/techacademy/katsuhito/muratomi/qa_app/Const.java
@@ -0,0 +1,14 @@
+package jp.techacademy.katsuhito.muratomi.qa_app;
+
+/**
+ * Created by katsu on 2017/04/26.
+ */
+
+public class Const {
+
+    public static final String UsersPATH="users";
+    public static final String ContentsPATH="contents";
+    public static final String AnswersPATH="answers";
+    public static final String NameKEY="name";
+
+}
